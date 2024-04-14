c487d2285e7ec2b26670a6cca9d4f881fa24abd1
Omar_Fakhredin <omarfe123@gmail.com>
2024-04-14 18:30:50 +0300

    CSS changes

--- OnlineDeployment/index.php
+++ OnlineDeployment/index.php
@@ -1,55 +1,58 @@
-<?php
-session_start();
-session_unset();
-session_destroy();
-?>
+<!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="css/my_style.css">
-                <link rel="stylesheet" href="css/mForm.css">       
-        <title>
-            Login
-        </title>
-    </head>
-    <body>
-        <div class="row header " >
-            
-        </div>
+
+<head>
+    <link rel="stylesheet" href="forms.css">
+    <title>Login</title>
+</head>
+
+<body>
+    <header style="text-align: center; margin-bottom: 20px;">
+        <h1>Welcome</h1>
+    </header>
+
+    <div class="container" style="width: 300px; margin: auto;">
         <div class="title">
-            <span>Login</h1>
-        </div>        
-        
-    
+            <h2>Login</h2>
+        </div>
+
         <div class="paragraph" style="border: 0;">
             <form action="Backend/login.php" method="POST" id="login-form">
-                <label for="un">User Name</label>
-                <br>
-                <input type="text" name="username" id="un" class="txtfield">
-                <br>
-                <label for="pass">Password</label>
-                <br>
-                <input type="password" name="password" id="pass"  class="txtfield">
-                <br>
-                <input type="button" value="Login" onclick="login()" class="mButton">
-                <input type="button" value="Cancel" onclick="ClearForm()" class="mButton">
+                <div class="form-group">
+                    <label for="un">User Name</label>
+                    <input type="text" name="username" id="un" class="txtfield">
+                </div>
+
+                <div class="form-group">
+                    <label for="pass">Password</label>
+                    <input type="password" name="password" id="pass" class="txtfield">
+                </div>
+
+                <div class="form-group">
+                    <input type="button" value="Login" onclick="login()" class="mButton">
+                    <input type="button" value="Cancel" onclick="ClearForm()" class="mButton">
+                </div>
             </form>
             <a href="html/signup.html">Sign Up...</a>
         </div>
-<script>
-    function login(){
-        var un=document.getElementById("un").value;
-        var pass=document.getElementById("pass").value;
-        if ((un=="")||(pass=="")){
-            alert("You must fill in the username and the password!");
-        }else{
-            document.getElementById("login-form").submit();
+    </div>
+
+    <script>
+        function login() {
+            var un = document.getElementById("un").value;
+            var pass = document.getElementById("pass").value;
+            if ((un == "") || (pass == "")) {
+                alert("You must fill in the username and the password!");
+            } else {
+                document.getElementById("login-form").submit();
+            }
         }
 
-    }
-    function ClearForm(){
-        document.getElementById("un").value="";
-        document.getElementById("pass").value="";
-    }
-</script>
+        function ClearForm() {
+            document.getElementById("un").value = "";
+            document.getElementById("pass").value = "";
+        }
+    </script>
 </body>
+
 </html>
